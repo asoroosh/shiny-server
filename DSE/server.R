98f8476df178e6a51063016b711db1c5efeacfe5
asoroosh <srafyouni@gmail.com>
2017-08-31 19:10:39 +0100

    HelloWorld

---
+++ DSE/server.R
@@ -0,0 +1,119 @@
+server<-function(input, output, session) {
+  
+  HCPMeta <- reactive({
+    S=round(readMat(paste('R/DSE_',as.character(input$HCP),'_FPP.mat',sep = ''))$S,digits=2);
+    D=round(readMat(paste('R/DSE_',as.character(input$HCP),'_FPP.mat',sep = ''))$D,digits=2);
+    nsub=dim(S)[1]
+    
+    PL=3
+    set.seed(1);
+    xtick=rnorm(nsub,mean = PL,sd = 0.02)
+
+    diffVar=abs(S-D);
+    Idx=diffVar<=quantile(abs(S-D),input$HCPSB/100)
+    
+    xtick=xtick[Idx]
+    S=S[Idx]
+    D=D[Idx]
+    
+    list("S"=S,
+         "D"=D,
+        "nsub"=nsub,
+        "xtick"=xtick,
+        "xcentre"=PL)
+  })
+  
+  #--------------------------------------------------------------------
+  output$out3 <- renderPlot({
+    HCPMeta=HCPMeta()
+    
+    Dcol=rgb(1, 0, 0, 0.5);
+    Scol=rgb(0, 0, 1, 0.5);
+    YlimTmp=c(20,80)
+    
+    plot(HCPMeta$xtick,HCPMeta$D,col = Dcol,xlim = c(HCPMeta$xcentre-.5,HCPMeta$xcentre+.5),ylim = YlimTmp ,ann=FALSE, xaxt='n',pch=16,cex=1)
+    par(new=TRUE)
+    plot(HCPMeta$xtick,HCPMeta$S,col = Scol,xlim = c(HCPMeta$xcentre-.5,HCPMeta$xcentre+.5),ylim = YlimTmp,ann=FALSE, xaxt='n',pch=16,cex=1)
+    grid(nx = 10, ny = 10, col = "lightgray", lty = "dotted")
+    par(new=TRUE)
+    plot(c(HCPMeta$xcentre-.5,HCPMeta$xcentre+.5),c(50,50),type = "l",xaxt='n',yaxt='n',xlab = 'Processing Level',ylab = '%A-var')
+    axis(1,at = HCPMeta$xcentre,labels = ('FPP'),xlim = c(HCPMeta$xcentre-.5,HCPMeta$xcentre+.5),ylim = YlimTmp)
+    })
+  #--------------------------------------------------------------------
+  output$out1 <- renderPrint({
+    HCPMeta=HCPMeta()
+    HCP_Into_texts=paste(input$HCP,' # of Subjects: ', HCPMeta$nsub,sep = '')
+    HCP_Into_texts},quoted = FALSE)
+  #--------------------------------------------------------------------
+  output$DHCPSum <- renderPrint({
+    HCPMeta=HCPMeta()
+    summary(HCPMeta$D)
+  })
+  output$SHCPSum <- renderPrint({
+    HCPMeta=HCPMeta()
+    summary(HCPMeta$S)
+  })
+  #----------------------------------------------------------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------------------------------------------------------
+  #----------------------------------------------------------------------------------------------------------------------------------------
+  
+  PCPMeta <- reactive({
+    S=round(readMat(paste('R/DSE_FPP_',as.character(input$PCP),'.mat',sep = ''))$S,digits=2);
+    D=round(readMat(paste('R/DSE_FPP_',as.character(input$PCP),'.mat',sep = ''))$D,digits=2);
+    nsub=dim(S)[1]
+    
+    set.seed(1);
+    xtick=rnorm(nsub,mean = 3,sd = 0.02)
+
+    S = S[,1]
+    D = D[,1]
+        
+    diffVar=abs(S-D);
+    Idx=diffVar<=quantile(abs(S-D),input$PCPSB/100)
+
+    xtick=xtick[Idx]
+    S=S[Idx]
+    D=D[Idx]
+    
+    list("S"=S,
+         "D"=D,
+         "nsub"=nsub,
+         "xtick"=xtick,
+         "xcentre"=3)
+  })
+  #--------------------------------------------------------------------
+  
+  output$out4 <- renderPlot({
+    PCPMeta=PCPMeta()
+        
+    PL=PCPMeta$xcentre; minPL=min(PL); maxPL=max(PL);
+    D=PCPMeta$D;
+    S=PCPMeta$S;
+    
+    Dcol=rgb(1, 0, 0, 0.5);
+    Scol=rgb(0, 0, 1, 0.5);
+    YlimTmp=c(20,80)
+    
+    plot(PCPMeta$xtick,D,col=Dcol,xlim = c(minPL-.5,maxPL+.5),ylim = YlimTmp ,ann=FALSE, xaxt='n',pch=16,cex=1)
+    par(new=TRUE)
+    plot(PCPMeta$xtick,S,col=Scol,xlim = c(minPL-.5,maxPL+.5),ylim = YlimTmp,ann=FALSE, xaxt='n',pch=16,cex=1)
+    grid(nx = 10, ny = 10, col = "lightgray", lty = "dotted")
+    par(new=TRUE)
+    plot(c(PCPMeta$xcentre-.5,PCPMeta$xcentre+.5),c(50,50),type = "l",xaxt='n',yaxt='n',xlab = 'Processing Level',ylab = '%A-var')
+    axis(1,at = PCPMeta$xcentre,labels = ('FPP'),xlim = c(minPL-.5,maxPL+.5),ylim = YlimTmp)
+  })
+  #--------------------------------------------------------------------
+  output$out2 <- renderText({
+    PCPMeta=PCPMeta()
+    PCP_Into_texts=paste(input$PCP,' # of Subjects: ', PCPMeta$nsub,sep = '')
+    PCP_Into_texts},quoted = FALSE)
+  #--------------------------------------------------------------------
+  output$DPCPSum <- renderPrint({
+    PCPMeta=PCPMeta()
+    summary(PCPMeta$D)
+  })
+  output$SPCPSum <- renderPrint({
+    PCPMeta=PCPMeta()
+    summary(PCPMeta$S)
+  })
+}
---
+++ DSE/ui.R
@@ -0,0 +1,79 @@
+fluidPage(
+  headerPanel("Data Quality Control by DSE Decomposition"),
+  br(),
+  fluidRow(
+    column(4,
+           h4("HCP"),
+           p("100 unrelated fully pre-processed (FPP) subjects of the Human Connectome Proeject. [date accessed: 15/8/2017]")
+           ),
+    column(6,
+           h4("ABIDE"),
+           p("530 healthy subjects across 20 acquisition sites from Autism Brain Imaging Data Exchange. \
+             Fully pre-processed images downloaded from Pre-processed Connectome Proejct (PCP). \
+             [date accessed: 15/8/2017]")
+           )
+    ),
+  fluidRow(
+    column(4,
+           hr(),
+           selectInput('HCP', 'Choose a dataset:', c('HCP'), selectize=FALSE,width = 400),
+           verbatimTextOutput('out1')
+    ),
+    column(6,
+           hr(),
+           selectInput('PCP', 'Choose a dataset:', c('CALTECH','CMU','KKI','LEUVEN_1','LEUVEN_2',
+                                                     'MAX_MUN','NYU','OHSU','OLIN','PITT','SBL',
+                                                     'SDSU','STANFORD','TRINITY','UCLA_1','UCLA_2',
+                                                     'UM_1','UM_2','USM','YALE'), selectize=FALSE,width = 650),
+           verbatimTextOutput('out2')
+    )
+  ),
+  fluidRow(
+    column(4,
+           hr(),
+           plotOutput("out3")
+    ),
+    column(6,
+           hr(),
+           plotOutput('out4')
+    )
+  ),  
+  fluidRow(
+    column(4,
+           hr(),
+           sliderInput("HCPSB","Percentile of %Svar-%Dvar:", min = 0, max = 100, value = 75,width = 400)
+    ),
+    column(6,
+           hr(),
+           sliderInput("PCPSB","Percentile of %Svar-%Dvar:", min = 0, max = 100, value = 75,width = 650)
+    )
+  ),  
+  fluidRow(
+    column(4,
+           hr(),
+           h6("%Dvar:"),
+           verbatimTextOutput("DHCPSum"),
+           h6("%Svar:"),
+           verbatimTextOutput("SHCPSum")           
+    ),
+    column(6,
+           hr(),
+           h6("%Dvar:"),
+           verbatimTextOutput("DPCPSum"),
+           h6("%Svar:"),
+           verbatimTextOutput("SPCPSum")           
+    )
+  ), 
+  fluidRow(
+    column(2),
+    column(6,
+           h5("Help us expand this page:"),
+           p("We are actively seeking to add other openly available data-sets to this page. Please contact us if you have any pre-processed data-sets and want to publicly evaluate their quality via DSE decomposition.\n
+              Email Soroosh Afyouni <srafyouni@gmail.com> or Thomas Nichols <thomas.nichols@bdi.ox.ac.uk>"),
+           h5("Citation:"),
+           p("Afyouni, Soroosh, and Thomas E. Nichols. Insight and Inference for DVARS. bioRxiv (2017). doi: https://doi.org/10.1101/125021"),
+           h6("http://nisox.org")
+           )
+    )
+  )
+  
